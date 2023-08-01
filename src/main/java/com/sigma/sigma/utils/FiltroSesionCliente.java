a3f7912b827b0d64b7896546ead3af62eb22f2cc
DiazPedroza_CarlosBenjamin <20223tn048@utez.edu.mx>
2023-08-01 00:46:33 -0600

    modificaciones Benjamin

---
+++ src/main/java/com/sigma/sigma/utils/FiltroSesionCliente.java
@@ -0,0 +1,56 @@
+package com.sigma.sigma.utils;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {
+        "/Cliente.jsp"
+})
+public class FiltroSesionCliente implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // Get the HttpSession from the request
+        HttpSession session = httpRequest.getSession(false);
+
+        // Check if the user is an admin(assuming isAdmin is a boolean session attribute)
+        boolean isAdmin = false;
+        if(session != null){
+
+            if (session.getAttribute("tipoSesion") != null) {
+                System.out.println(session.getAttribute("tipoSesion"));
+                String op = session.getAttribute("tipoSesion").toString();
+                isAdmin = session.getAttribute("tipoSesion").equals(op);
+            }
+
+        }
+
+        if (isAdmin) {
+            // If the user is an admin, allow access to the Servlet
+            chain.doFilter(request, response);
+        } else {
+            // If the user is not an admin, redirect them to a restricted access page
+            httpResponse.sendRedirect("FailedAccess.jsp");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup code if needed (optional)
+    }
+}
---
+++ src/main/java/com/sigma/sigma/utils/FiltroSesionGerente.java
@@ -0,0 +1,63 @@
+package com.sigma.sigma.utils;
+
+import com.sigma.sigma.model.LoginDao;
+import com.sigma.sigma.model.Usuario;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {
+        "/Gerente.jsp",
+        "/RegistrarEmpleado.jsp",
+        "/RegistrarUsuario.jsp",
+        "/RegistroArticulos.jsp",
+        "/ModificarEmpleado.jsp"
+})
+public class FiltroSesionGerente implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // Get the HttpSession from the request
+        HttpSession session = httpRequest.getSession(false);
+
+        // Check if the user is an admin(assuming isAdmin is a boolean session attribute)
+        boolean isAdmin = false;
+        if(session != null){
+
+            if (session.getAttribute("tipoSesion") != null) {
+
+                System.out.println(session.getAttribute("tipoSesion"));
+                isAdmin = session.getAttribute("tipoSesion").equals("Gerente");
+            }
+
+        }
+
+        if (isAdmin) {
+            // If the user is an admin, allow access to the Servlet
+            chain.doFilter(request, response);
+        } else {
+            // If the user is not an admin, redirect them to a restricted access page
+            httpResponse.sendRedirect("FailedAccess.jsp");
+        }
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup code if needed (optional)
+    }
+}
